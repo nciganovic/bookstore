97e74e05c7a96310a0e8307602ba976f91d3c600
nciganovic <nciganovic99@gmail.com>
2020-07-28 16:59:10 +0200

    Add SQL Server Database to project

--- BookStore/BookStore/Startup.cs
+++ BookStore/BookStore/Startup.cs
@@ -31,7 +31,7 @@ namespace BookStore
                 options => options.UseSqlServer(conf.GetConnectionString("BookStoreDbConnection")));
 
             services.AddMvc(options => options.EnableEndpointRouting = false);
-            services.AddSingleton<IPersonRepository, MockPersonRepository>();
+            services.AddScoped<IPersonRepository, SqlPersonRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
