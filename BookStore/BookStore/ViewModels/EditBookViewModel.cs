19a808ab5214313d491789b1aab642eacff53c6c
nciganovic <nciganovic99@gmail.com>
2020-07-31 23:10:15 +0200

    ViewModel for EditBook

---
+++ BookStore/BookStore/ViewModels/EditBookViewModel.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.Models;
+using BookStore.Models.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.ViewModels
+{
+    public class EditBookViewModel
+    {
+        public IEnumerable<Category> AllCategories { get; set; }
+        public Book Book { get; set; }
+    }
+}
