e7094f8cf130014b7ef6388ead53879998ec4596
nciganovic <nciganovic99@gmail.com>
2020-07-31 13:52:23 +0200

    Add Book Controller

---
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BookStore.Models.Tables;
+using BookStore.Models.InterfaceRepo;
+using BookStore.Models;
+using BookStore.ViewModels;
+using BookStore.Models.Dto;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private IBookRepository bookRepository;
+        private ICategoryRepository categoryRepository;
+
+        public BookController(IBookRepository bookRepository, ICategoryRepository categoryRepository) {
+            this.categoryRepository = categoryRepository;
+            this.bookRepository = bookRepository;
+        }
+
+        [Route("Admin/Books")]
+        public ViewResult DisplayAllBooks() {
+            IEnumerable<GetBookDto> allBooks = bookRepository.GetAllBooks();
+            ViewBag.Title = "All books";
+            return View("Views/Admin/Book/DisplayAllBooks.cshtml", allBooks);
+        }
+
+        [HttpGet]
+        [Route("Admin/Books/Create")]
+        public ViewResult CreateBook()
+        {
+            IEnumerable<Category> allCategories = categoryRepository.GetAllCategories();
+            CreateBookViewModel viewModel = new CreateBookViewModel
+            {
+                AllCategories = allCategories,
+                Book = new Book()
+            };
+            return View("Views/Admin/Book/CreateBook.cshtml", viewModel);
+        }
+
+        [HttpPost]
+        [Route("Admin/Books/Create")]
+        public IActionResult CreateBook(Book book)
+        {
+            if (ModelState.IsValid) {
+                bookRepository.Add(book);
+                return RedirectToAction("DisplayAllBooks");
+            }
+
+            IEnumerable<Category> allCategories = categoryRepository.GetAllCategories();
+            CreateBookViewModel viewModel = new CreateBookViewModel {
+                AllCategories = allCategories,
+                Book = new Book()
+            };
+
+            return View("Views/Admin/Book/CreateBook.cshtml", viewModel);
+        }
+    }
+}
