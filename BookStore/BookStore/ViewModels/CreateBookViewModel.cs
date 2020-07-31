872d87a19a5028145d980cbe1cfd84b7bc76b0e0
nciganovic <nciganovic99@gmail.com>
2020-07-31 13:52:01 +0200

    Add Views for Books

---
+++ BookStore/BookStore/ViewModels/CreateBookViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Models;
+using BookStore.Models.Tables;
+
+namespace BookStore.ViewModels
+{
+    public class CreateBookViewModel
+    {
+        public Book Book { get; set; }
+        public IEnumerable<Category> AllCategories { get; set; } 
+    }
+}
