6e0c0bf284993e772c1def237b67b0cb10743d35
nciganovic <nciganovic99@gmail.com>
2020-08-02 17:32:53 +0200

    Added rotes for CRUD AuthorBook Table

---
+++ BookStore/BookStore/Controllers/AuthorBookController.cs
@@ -0,0 +1,100 @@
+﻿using BookStore.Models;
+using BookStore.Models.Dto;
+using BookStore.Models.InterfaceRepo;
+using BookStore.Models.Tables;
+using BookStore.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class AuthorBookController : Controller
+    {
+        private IAuthorBookRepository authorBookRepository;
+        private IAuthorRepository authorRepository;
+        private IBookRepository bookRepository;
+
+        public AuthorBookController(IAuthorBookRepository authorBookRepository, IAuthorRepository authorRepository, IBookRepository bookRepository) {
+            this.authorRepository = authorRepository;
+            this.authorBookRepository = authorBookRepository;
+            this.bookRepository = bookRepository;
+        }
+
+        [Route("Admin/AuthorBook")]
+        public IActionResult DisplayAllAuthorBooks() {
+            var allAuthorBooks = authorBookRepository.GetAllAuthorBooks();
+            ViewBag.Title = "All author books";
+            return View("Views/Admin/AuthorBook/DisplayAllAuthorBooks.cshtml", allAuthorBooks);
+        }
+
+        [HttpGet]
+        [Route("Admin/AuthorBook/Create")]
+        public IActionResult CreateAuthorBook()
+        {
+            AuthorBookViewModel viewModel = GetAuthorBookViewModel(new AuthorBook());
+            return View("Views/Admin/AuthorBook/CreateAuthorBook.cshtml", viewModel);
+        }
+
+        [HttpPost]
+        [Route("Admin/AuthorBook/Create")]
+        public IActionResult CreateAuthorBook(AuthorBook authorBook)
+        {
+            if (ModelState.IsValid) {
+                authorBookRepository.Add(authorBook);
+                return RedirectToAction("DisplayAllAuthorBooks");
+            }
+
+            AuthorBookViewModel viewModel = GetAuthorBookViewModel(new AuthorBook());
+            return View("Views/Admin/AuthorBook/CreateAuthorBook.cshtml", viewModel);
+        }
+
+        private AuthorBookViewModel GetAuthorBookViewModel(AuthorBook authorBook)
+        {
+            IEnumerable<GetAuthorDto> allAuthors = authorRepository.GetAllAuthors();
+            IEnumerable<GetBookDto> allBooks = bookRepository.GetAllBooks();
+            AuthorBookViewModel viewModel = new AuthorBookViewModel
+            {
+                AllAuthors = allAuthors,
+                AllBooks = allBooks,
+                AuthorBook = authorBook
+            };
+
+            return viewModel;
+        }
+
+        [HttpGet]
+        [Route("Admin/AuthorBook/Edit/{id}")]
+        public IActionResult EditAuthorBook(int id)
+        {
+            AuthorBook editAuthorBook = authorBookRepository.GetAuthorBook(id);
+            AuthorBookViewModel viewModel = GetAuthorBookViewModel(editAuthorBook);
+            return View("Views/Admin/AuthorBook/EditAuthorBook.cshtml", viewModel);
+        }
+
+        [HttpPost]
+        [Route("Admin/AuthorBook/Edit/{id}")]
+        public IActionResult EditAuthorBook(AuthorBook authorBook)
+        {
+            if (ModelState.IsValid)
+            {
+                authorBookRepository.Update(authorBook);
+                return RedirectToAction("DisplayAllAuthorBooks");
+            }
+
+            AuthorBook editAuthorBook = authorBookRepository.GetAuthorBook(authorBook.Id);
+            AuthorBookViewModel viewModel = GetAuthorBookViewModel(editAuthorBook);
+            return View("Views/Admin/AuthorBook/EditAuthorBook.cshtml", viewModel);
+        }
+
+        [Route("Admin/AuthorBook/Delete/{id}")]
+        public IActionResult DeleteAuthorBook(int id)
+        {
+            authorBookRepository.Delete(id);
+            return RedirectToAction("DisplayAllAuthorBooks");
+        }
+
+    }
+}
