f956f4164c448bd8ca4bed7abd59c7c3f2a23673
nciganovic <nciganovic99@gmail.com>
2020-08-02 17:35:19 +0200

    Dto for AuthorBook table

---
+++ BookStore/BookStore/Models/Dto/GetAuthorBookDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.Dto
+{
+    public class GetAuthorBookDto
+    {
+        public int Id { get; set; }
+        public string AuthorName { get; set; }
+        public string BookTitle { get; set; }
+    }
+}
