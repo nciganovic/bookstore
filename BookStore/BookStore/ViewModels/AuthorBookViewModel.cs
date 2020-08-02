6d61c74119d221228f6ce76e3db51903a34be782
nciganovic <nciganovic99@gmail.com>
2020-08-02 17:34:33 +0200

    ViewModel for AdminBookConroller

---
+++ BookStore/BookStore/ViewModels/AuthorBookViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BookStore.Models;
+using BookStore.Models.Dto;
+using BookStore.Models.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.ViewModels
+{
+    public class AuthorBookViewModel
+    {
+        public IEnumerable<GetAuthorDto> AllAuthors { get; set; }
+        public IEnumerable<GetBookDto> AllBooks { get; set; }
+        public AuthorBook AuthorBook { get; set; }
+    }
+}
