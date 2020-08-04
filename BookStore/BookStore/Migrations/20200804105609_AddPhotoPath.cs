ff5fc73192e949914ae91acf767ddd60ceb0971c
nciganovic <nciganovic99@gmail.com>
2020-08-04 15:02:37 +0200

    migrations for PhotoPath

---
+++ BookStore/BookStore/Migrations/20200804105609_AddPhotoPath.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookStore.Migrations
+{
+    public partial class AddPhotoPath : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoPath",
+                table: "Books",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoPath",
+                table: "Books");
+        }
+    }
+}
