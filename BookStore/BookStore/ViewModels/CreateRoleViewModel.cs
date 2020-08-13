0b582c34e8466ae3d06b4f4a6c9cc928c729fb43
nciganovic <nciganovic99@gmail.com>
2020-08-13 11:59:28 +0200

    Create role View and ViewModel

---
+++ BookStore/BookStore/ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        [Display(Name = "Role name")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only.")]
+        public string RoleName { get; set; }
+    }
+}
