e4411b808493968a2208f0f76f97a8cbcbb30439
nciganovic <nciganovic99@gmail.com>
2020-08-13 13:44:35 +0200

    Views to Edit Roles

---
+++ BookStore/BookStore/ViewModels/EditRoleViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.ViewModels
+{
+    public class EditRoleViewModel
+    {
+        public EditRoleViewModel()
+        {
+           Users = new List<string>();
+        }
+
+        public string Id { get; set; }
+        [Required]
+        [Display(Name = "Role name")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only.")]
+        public string RoleName { get; set; }
+        public List<string> Users { get; set; }
+    }
+}
